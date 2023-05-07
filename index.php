bf5b431755cc40dd75ca758f592dff21a0eba8b2
Alexander Kostin <asurv2@gmail.com>
2023-05-07 19:39:02 +0300

    create file

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include('src/config.php'); ?>
+<!DOCTYPE html>
+<html lang="uk-UK">
+<head>
+  <?php include "includes/header.php"; ?>
+</head>
+<body
+  class="home page-template page-template-elementor_canvas page page-id-12 wp-custom-logo e-lazyload elementor-default elementor-template-canvas elementor-kit-14 elementor-page elementor-page-12">
+  <?php include "includes/svg_pack.php"; ?>
+  <div data-elementor-type="wp-page" data-elementor-id="12" class="elementor elementor-12">
+    <!-- <div class="container"> -->
+    <?php include "includes/hero.php"; ?>
+    <?php include "includes/price.php"; ?>
+    <?php include "includes/price_accord.php"; ?>
+    <?php include "includes/portfolio.php"; ?>
+    <?php include "includes/photo_workers.php"; ?>
+    <?php include "includes/registration.php"; ?>
+    <?php include "includes/review.php"; ?>
+    <?php include "includes/our_advantages.php"; ?>
+    <?php include "includes/location.php"; ?>
+    <?php include "includes/footer.php"; ?>
+    <?php include "includes/scripts_link_old.php"; ?>
+    <!-- </div> -->
+  </div>
+</body>
+</html>
