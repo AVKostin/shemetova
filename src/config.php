ccb69a735a6731b55680789ccd67188e04894e2e
Alexander Kostin <asurv2@gmail.com>
2023-05-07 19:42:38 +0300

    create file

---
+++ src/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$config = array(
+  'title' => 'Тестовый прайс на PHP :)',
+  'db' => array(
+    'server' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'name' => 'shem_stud'
+  )
+);
+
+require "db.php";
