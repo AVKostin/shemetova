bdab6e99c5bb40e56db2bd9c30fde39ab59f3e94
Alexander Kostin <asurv2@gmail.com>
2023-05-07 19:45:03 +0300

    create file

---
+++ includes/our_advantages.php
@@ -0,0 +1,30 @@
+<div class="container">
+  <section class="why-we_section" title="Наші переваги">
+
+    <div class="elementor-element elementor-element-8a7bdbd elementor-widget elementor-widget-bdt-advanced-heading"
+      data-id="8a7bdbd" data-element_type="widget" data-widget_type="bdt-advanced-heading.default">
+      <div class="elementor-widget-container">
+        <div id="8a7bdbd" class="bdt-ep-advanced-heading" data-settings="{&quot;titleMultiColor&quot;:&quot;no&quot;}">
+          <div class="bdt-ep-advanced-heading-content bdt-visible@m">
+            <div class="bdt-transform-origin-top-left">Shemetova Studio</div>
+          </div>
+
+          <div class="bdt-ep-advanced-heading-sub-title">
+            <div class="bdt-ep-advanced-heading-sub-title-content">Чому саме ми?</div>
+          </div>
+          <h2 class="bdt-ep-advanced-heading-title"><span class="bdt-ep-advanced-heading-main-title"><span
+                class="bdt-ep-advanced-heading-main-title-inner">Наші переваги</span></span></h2>
+        </div>
+      </div>
+    </div>
+
+    <ul class="why-we_list list">
+      <li class="why-we_item">Безкоштовна кава, чай; </li>
+      <li class="why-we_item">100% стерилізація інструменту;</li>
+      <li class="why-we_item">Працюємо навіть без світла;</li>
+      <li class="why-we_item"> Досвідчені майстри;</li>
+      <li class="why-we_item"> В індустрії краси з 2015 року;</li>
+      <li class="why-we_item"> Затишна атмосфера...</li>
+    </ul>
+  </section>
+</div>
