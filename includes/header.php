84d4a783e76adcec43a96a86e90075da9488d919
Alexander Kostin <asurv2@gmail.com>
2023-05-07 19:44:06 +0300

    create file

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=5.0">
+  <title>Салон Краси "Шеметова Студио" на Троєщині | Shemetova Studio — Студія Краси | Троєщина</title>
+<meta name="robots" content="max-image-preview:large">
+<link href="css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+<link rel="stylesheet" id="bdt-uikit-css" href="css/bdt-uikit.css" media="all">
+<link rel="stylesheet" id="ep-helper-css" href="css/ep-helper.css" media="all">
+<link rel="stylesheet" id="classic-theme-styles-css" href="css/global-styles-inline-css.css" media="all">
+
+<link rel="stylesheet"
+  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+<link rel="stylesheet" href="css/mstyle.css">
+<link rel="stylesheet" href="css/css_temp.css">
+<link rel="stylesheet" id="hello-elementor-css" href="css/style.min.css" media="all">
+<link rel="stylesheet" id="elementor-frontend-css" href="css/frontend-lite.min_1.css" media="all">
+<link rel="stylesheet" id="elementor-post-14-css" href="css/post-14.css" media="all">
+<link rel="stylesheet" id="elementor-lazyload-css" href="css/frontend.min.css" media="all">
+<link rel="stylesheet" id="swiper-css" href="css/swiper.min.css" media="all">
+<link rel="stylesheet" id="elementor-global-css" href="css/global.css" media="all">
+<link rel="stylesheet" id="elementor-post-12-css" href="css/post-12.css" media="all">
+<!-- <link rel="stylesheet" id="google-fonts-1-css"
+  href="https://fonts.googleapis.com/css?family=Roboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto+Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CLobster%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&amp;display=swap&amp;subset=cyrillic"
+  media="all"> -->
+<link rel="canonical" href="https://shemetova.studio/">
+<meta name="generator"
+  content="Elementor 3.11.5; features: e_dom_optimization, e_optimized_assets_loading, e_optimized_css_loading, e_font_icon_svg, a11y_improvements, additional_custom_breakpoints; settings: css_print_method-external, google_font-enabled, font_display-swap">
+<link rel="icon" href="images/nail-polish-150x150.webp" sizes="32x32">
+<link rel="icon" href="images/nail-polish.webp" sizes="192x192">
+<link rel="apple-touch-icon" href="images/nail-polish.webp">
+<meta name="msapplication-TileImage" content="images/nail-polish.webp">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
