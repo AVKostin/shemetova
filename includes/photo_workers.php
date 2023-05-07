1612a72a5a3f9d6bc0af7dc3196a69977f40c9a1
Alexander Kostin <asurv2@gmail.com>
2023-05-07 19:45:46 +0300

    create file

---
+++ includes/photo_workers.php
@@ -0,0 +1,121 @@
+ <div class="container">
+   <div class="elementor-element elementor-element-d17795f e-con-boxed e-con" data-id="d17795f"
+     data-element_type="container" data-settings="{&quot;content_width&quot;:&quot;boxed&quot;}">
+     <div class="e-con-inner">
+       <div class="elementor-element elementor-element-8a7bdbd elementor-widget elementor-widget-bdt-advanced-heading"
+         data-id="8a7bdbd" data-element_type="widget" data-widget_type="bdt-advanced-heading.default">
+         <div class="elementor-widget-container">
+           <div id="8a7bdbd" class="bdt-ep-advanced-heading"
+             data-settings="{&quot;titleMultiColor&quot;:&quot;no&quot;}">
+             <div class="bdt-ep-advanced-heading-content bdt-visible@m">
+               <div class="bdt-transform-origin-top-left">Shemetova Studio</div>
+             </div>
+             <div class="bdt-ep-advanced-heading-sub-title">
+               <div class="bdt-ep-advanced-heading-sub-title-content">Наш персонал</div>
+             </div>
+             <h2 class="bdt-ep-advanced-heading-title"><span class="bdt-ep-advanced-heading-main-title"><span
+                   class="bdt-ep-advanced-heading-main-title-inner">Фото наших
+                   майстрів</span></span></h2>
+           </div>
+         </div>
+       </div>
+       <div>
+         <div id="carouselExample" class="carousel slide">
+           <div class="carousel-inner k_personal">
+             <div class="carousel-item active">
+               <div class="row">
+                 <div class="col-md-4 k_img_for_big col-s-12"><img src="./images/k1.webp" alt="our masters"
+                     loading="lazy"></div>
+                 <div class="col-md-4 k_img_for_smal col-s-0"><img src="./images/k2.webp" alt="our masters"
+                     loading="lazy"></div>
+                 <div class="col-md-4 k_img_for_smal col-s-0"><img src="./images/k3.webp" alt="our masters"
+                     loading="lazy"></div>
+               </div>
+             </div>
+             <div class="carousel-item row">
+               <div class="row" style="margin: 0; padding: 0;">
+                 <div style="padding: 0;" class="col-md-4 k_img_for_big col-s-12"><img src="./images/k2.webp"
+                     alt="our masters" loading="lazy"></div>
+                 <div class="col-md-4 k_img_for_smal col-s-0"><img src="./images/k3.webp" alt="our masters"
+                     loading="lazy"></div>
+                 <div class="col-md-4 k_img_for_smal col-s-0"><img src="./images/k4.webp" alt="our masters"
+                     loading="lazy"></div>
+               </div>
+             </div>
+             <div class="carousel-item">
+               <div class="row">
+                 <div class="col-md-4 k_img_for_big col-s-12"><img src="./" alt="our masters" loading="lazy"
+                     loading="lazy"></div>
+                 <div class="col-md-4 k_img_for_smal col-s-0"><img src="./images/k4.webp" alt="our masters"
+                     loading="lazy"></div>
+                 <div class="col-md-4 k_img_for_smal col-s-0"><img src="./images/k5.webp" alt="our masters"
+                     loading="lazy"></div>
+               </div>
+             </div>
+             <div class="carousel-item">
+               <div class="row">
+                 <div class="col-md-4 k_img_for_big col-s-12"><img src="./images/k4.webp" alt="our masters"
+                     loading="lazy"></div>
+                 <div class="col-md-4 k_img_for_smal col-s-0"><img src="./images/k5.webp" alt="our masters"
+                     loading="lazy"></div>
+                 <div class="col-md-4 k_img_for_smal col-s-0"><img src="./images/k6.webp" alt="our masters"
+                     loading="lazy"></div>
+               </div>
+             </div>
+             <div class="carousel-item">
+               <div class="row">
+                 <div class="col-md-4 k_img_for_big col-s-12"><img src="./images/k5.webp" alt="our masters"
+                     loading="lazy"></div>
+                 <div class="col-md-4 k_img_for_smal col-s-0"><img src="./images/k6.webp" alt="our masters"
+                     loading="lazy"></div>
+                 <div class="col-md-4 k_img_for_smal col-s-0"><img src="./images/k7.webp" alt="our masters"
+                     loading="lazy"></div>
+               </div>
+             </div>
+             <div class="carousel-item">
+               <div class="row">
+                 <div class="col-md-4 k_img_for_big col-s-12"><img src="./images/k6.webp" alt="our masters"
+                     loading="lazy"></div>
+                 <div class="col-md-4 k_img_for_smal col-s-0"><img src="./images/k7.webp" alt="our masters"
+                     loading="lazy"></div>
+                 <div class="col-md-4 k_img_for_smal col-s-0"><img src="./images/k8.webp" alt="our masters"
+                     loading="lazy"></div>
+               </div>
+             </div>
+             <div class="carousel-item">
+               <div class="row">
+                 <div class="col-md-4 k_img_for_big col-s-12"><img src="./images/k8.webp" alt="our masters"
+                     loading="lazy"></div>
+                 <div class="col-md-4 k_img_for_smal col-s-0"><img src="./images/k1.webp" alt="our masters"
+                     loading="lazy"></div>
+                 <div class="col-md-4 k_img_for_smal col-s-0"><img src="./images/k2.webp" alt="our masters"
+                     loading="lazy"></div>
+               </div>
+             </div>
+           </div>
+           <div class="display: flex; align-items: center;">
+             <button style="border: none; color: #591A2F; background-color: #591A2F; padding: 0; height: 70px;"
+               class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+               <span class="carousel-control-prev-icon" style="margin-top: 7px;" aria-hidden="true"></span>
+               <span class="visually-hidden">Previous</span>
+             </button>
+           </div>
+           <button style="border: none; color: #591A2F; background-color: #591A2F; padding: 0; height: 70px;"
+             class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+             <span class="carousel-control-next-icon" aria-hidden="true"></span>
+             <span class="visually-hidden">Next</span>
+           </button>
+         </div>
+       </div>
+     </div>
+   </div>
+   <div class="elementor-element elementor-element-3003fe6 e-con-full e-con" data-id="3003fe6"
+     data-element_type="container" data-settings="{&quot;content_width&quot;:&quot;full&quot;}">
+     <div class="elementor-element elementor-element-7e2331b elementor-widget elementor-widget-shortcode"
+       data-id="7e2331b" data-element_type="widget" data-widget_type="shortcode.default">
+       <div class="elementor-widget-container">
+         <div class="elementor-shortcode"></div>
+       </div>
+     </div>
+   </div>
+ </div>
