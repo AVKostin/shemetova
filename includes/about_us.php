fa0ebebbdb192b917b9234e105dbdd68f94e812a
Alexander Kostin <asurv2@gmail.com>
2023-05-08 16:03:54 +0300

    Update

---
+++ includes/about_us.php
@@ -0,0 +1,32 @@
+<div class="container">
+  <section class="about_us_section" title="Наші переваги">
+
+    <div class="elementor-element elementor-element-8a7bdbd elementor-widget elementor-widget-bdt-advanced-heading"
+      data-id="8a7bdbd" data-element_type="widget" data-widget_type="bdt-advanced-heading.default">
+      <div class="elementor-widget-container">
+        <div id="8a7bdbd" class="bdt-ep-advanced-heading" data-settings="{&quot;titleMultiColor&quot;:&quot;no&quot;}">
+          <div class="bdt-ep-advanced-heading-content bdt-visible@m">
+            <div class="bdt-transform-origin-top-left">Shemetova Studio</div>
+          </div>
+
+          <div class="bdt-ep-advanced-heading-sub-title">
+            <div class="bdt-ep-advanced-heading-sub-title-content">Будемо знайомі</div>
+          </div>
+          <h2 class="bdt-ep-advanced-heading-title"><span class="bdt-ep-advanced-heading-main-title"><span
+                class="bdt-ep-advanced-heading-main-title-inner">Коротко про нас</span></span></h2>
+        </div>
+      </div>
+    </div>
+
+    <div class="about_us_item">
+      <p class="about_us_text">
+      Вас вітає студія краси “Shemetova studio”!<br>
+Ми працюємо з 2016 року та  даруємо красу найпрекраснішим жінкам нашого міста.<br>
+Чому ви маєте обрати саме НАС?!<br>
+ У нас працюють найкращі майстри  манікюру, педикюру, бровісти, майстер по нарощуванню вій а також косметолог.<br>
+Ми використовуємо лише якісні матеріали, стерилізовані інструменти, одноразові розхідники та косметику преміум якості.<br>
+Наші адміністратори допоможуть обрати Вам зручний час та пригостять смачною кавою чи чаєм.<br>
+Наша команда хоче дарувати Вам красу та щиру посмішку.
+</p> </div>
+  </section>
+</div>
