91674147ad81d40887b498f03425ca1a486e4051
Alexander Kostin <asurv2@gmail.com>
2023-05-12 16:49:00 +0300

    Update code

--- includes/price_main.php
+++ includes/price_main.php
@@ -1,19 +1,13 @@
-<div class="container">
+<div class="container_main">
     <div
     class="elementor-element elementor-element-2bbe2fb elementor-hidden-mobile elementor-widget elementor-widget-bdt-tabs"
     data-id="2bbe2fb" data-element_type="widget" data-widget_type="bdt-tabs.default">
     <div class="elementor-widget-container">
-
-
       <div class="bdt-tabs-area">
-
         <div id="bdt-tabs-2bbe2fb" class="bdt-tabs  bdt-tabs-default" data-settings="{"
           id":"bdt-tabs-2bbe2fb","activeHash":"no","hashTopOffset":70,"hashScrollspyTime":1500,"navStickyOffset":1,"linkWidgetId":"2bbe2fb"}">
+              <?php include "includes/price_menu.php"; ?>
 
-          <!--  шапка прайса и меню -->
-          <?php include "includes/price_menu.php"; ?>
-
-          <div class="bdt-switcher-wrapper">
             <div id="bdt-tab-content-2bbe2fb" class="bdt-switcher bdt-switcher-item-content">
 
               <?php include "includes/works/manicure/manicure.php"; ?>
@@ -21,10 +15,7 @@
               <?php include "includes/works/makeup/makeup.php"; ?>
               <?php include "includes/works/eyebrows/eyebrows.php"; ?>
               <?php include "includes/works/cosmetology/cosmetology.php"; ?>
-              <!--  end of main price -->
             </div>
-          </div>
-
           <a href="#" id="bottom-anchor-2bbe2fb" data-bdt-hidden></a>
         </div>
       </div>
