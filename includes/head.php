bb9b791ecb2d8a20b936868836d36f1f7146d303
Alexander Kostin <asurv2@gmail.com>
2023-05-12 16:48:06 +0300

    Update code

---
+++ includes/head.php
@@ -0,0 +1,23 @@
+<meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=5.0">
+  <title>Салон Краси "Шеметова Студио" на Троєщині | Shemetova Studio Troeschina</title>
+  <meta name="description" content="Мы студія краси “Shemetova studio”. З 2015 року даруємо красу чарівним жінкам нашого міста. У нас працюють найкращі майстри  манікюру, педикюру, бровісти, майстер по нарощуванню вій а також косметолог. Ми використовуємо лише якісні матеріали, стерилізовані інструменти, одноразові розхідники та косметику преміум якості.
+">
+<meta name="keywords" content="манікюр, педикюр, бровіст, нарощення вій, косметолог">
+<meta name="author" content="Alex Kos - DevStudio">
+<meta name="publisher" content="Alex Kos - DevStudio">
+<meta name ="revised" content ="ShemetovaStudio by AlexKos, 08/05/2023" >
+<meta name="robots" content="max-image-preview:large">
+<meta name="generator"
+  content="Elementor 3.11.5; features: e_dom_optimization, e_optimized_assets_loading, e_optimized_css_loading, e_font_icon_svg, a11y_improvements, additional_custom_breakpoints; settings: css_print_method-external, google_font-enabled, font_display-swap">
+<meta name="msapplication-TileImage" content="images/nail-polish.webp">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WVHFSFM');</script>
+<!-- End Google Tag Manager -->
+<?php include_once "includes/link.php"; ?>
