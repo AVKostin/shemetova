3492f1071243e072e3dfa5dabdc6221093cd9040
Alexander Kostin <asurv2@gmail.com>
2023-05-12 16:48:25 +0300

    Update code

--- includes/location.php
+++ includes/location.php
@@ -1,9 +1,9 @@
-<div class="container">
-<section style="padding: 30px 3% 50px 3%;">
+<div class="container_main">
+<section style="padding: 30px 3% 50px 3%;" title="Our location on maps">
    <div class="elementor-element elementor-element-8a7bdbd elementor-widget elementor-widget-bdt-advanced-heading"
      data-id="8a7bdbd" data-element_type="widget" data-widget_type="bdt-advanced-heading.default">
      <div class="elementor-widget-container">
-       <div id="8a7bdbd" class="bdt-ep-advanced-heading" data-settings="{&quot;titleMultiColor&quot;:&quot;no&quot;}">
+       <div  class="bdt-ep-advanced-heading" data-settings="{&quot;titleMultiColor&quot;:&quot;no&quot;}">
          <div class="bdt-ep-advanced-heading-content bdt-visible@m">
            <div class="bdt-transform-origin-top-left">Shemetova Studio</div>
          </div>
@@ -18,7 +18,7 @@
    <iframe
      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d40593.896012198944!2d30.537587298063624!3d50.51336188528439!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4d1ee2b1e61a9%3A0x8f0153b169ac9475!2sShemetova%20Studio!5e0!3m2!1suk!2sua!4v1679516261892!5m2!1suk!2sua"
      width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-     referrerpolicy="no-referrer-when-downgrade"></iframe>
+     referrerpolicy="no-referrer-when-downgrade" title="our location on google maps"></iframe>
  </section>
 </div>
 
