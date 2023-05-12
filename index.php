ac58a9416cf56d411c10065f3c21513627d33591
Alexander Kostin <asurv2@gmail.com>
2023-05-12 16:47:54 +0300

    Update code

--- index.php
+++ index.php
@@ -1,27 +1,32 @@
 <?php include('src/config.php'); ?>
 <!DOCTYPE html>
-<html lang="uk-UK">
+<html lang="uk-UA">
 <head>
-  <?php include "includes/header.php"; ?>
+  <?php include "includes/head.php"; ?>
 </head>
+
 <body
   class="home page-template page-template-elementor_canvas page page-id-12 wp-custom-logo e-lazyload elementor-default elementor-template-canvas elementor-kit-14 elementor-page elementor-page-12">
-  <?php include "includes/svg_pack.php"; ?>
-  <div data-elementor-type="wp-page" data-elementor-id="12" class="elementor elementor-12">
-    <!-- <div class="container"> -->
-    <?php include "includes/hero.php"; ?>
-    <?php include "includes/about_us.php"; ?>
-    <?php include "includes/price.php"; ?>
-    <?php include "includes/price_accord.php"; ?>
-    <?php include "includes/portfolio.php"; ?>
-    <?php include "includes/photo_workers.php"; ?>
-    <?php include "includes/registration.php"; ?>
-    <?php include "includes/review.php"; ?>
-    <?php include "includes/our_advantages.php"; ?>
-    <?php include "includes/location.php"; ?>
-    <?php include "includes/scripts_link_old.php"; ?>
-    <!-- </div> -->
-    <?php include "includes/footer.php"; ?>
-  </div>
+
+  <div class="container_main">
+      <div class="elementor elementor-12">
+  <header class="header_main">
+  <?php include "includes/hero.php"; ?>
+  </header>
+        <main>
+        <?php include "includes/about_us.php"; ?>
+        <?php include "includes/price.php"; ?>
+        <?php include "includes/price_accord.php"; ?>
+        <?php include "includes/portfolio.php"; ?>
+        <?php include "includes/photo_workers.php"; ?>
+        <?php include "includes/registration.php"; ?>
+        <?php include "includes/review.php"; ?>
+        <?php include "includes/our_advantages.php"; ?>
+        <?php include "includes/location.php"; ?>
+        </main>
+        <?php include "includes/footer.php"; ?>
+      </div>
+    </div>
+    <?php include "includes/scripts_link.php"; ?>
 </body>
 </html>
