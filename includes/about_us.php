e64c23d78ed2673cdf74a48b1bb30757d7795d55
Alexander Kostin <asurv2@gmail.com>
2023-05-12 16:47:42 +0300

    Update code

--- includes/about_us.php
+++ includes/about_us.php
@@ -1,10 +1,10 @@
-<div class="container">
+<div class="container_main">
   <section class="about_us_section" title="Наші переваги">
 
     <div class="elementor-element elementor-element-8a7bdbd elementor-widget elementor-widget-bdt-advanced-heading"
       data-id="8a7bdbd" data-element_type="widget" data-widget_type="bdt-advanced-heading.default">
       <div class="elementor-widget-container">
-        <div id="8a7bdbd" class="bdt-ep-advanced-heading" data-settings="{&quot;titleMultiColor&quot;:&quot;no&quot;}">
+        <div  class="bdt-ep-advanced-heading" data-settings="{&quot;titleMultiColor&quot;:&quot;no&quot;}">
           <div class="bdt-ep-advanced-heading-content bdt-visible@m">
             <div class="bdt-transform-origin-top-left">Shemetova Studio</div>
           </div>
@@ -19,14 +19,10 @@
     </div>
 
     <div class="about_us_item">
-      <p class="about_us_text">
-      Вас вітає студія краси “Shemetova studio”!<br>
-Ми працюємо з 2016 року та  даруємо красу найпрекраснішим жінкам нашого міста.<br>
-Чому ви маєте обрати саме НАС?!<br>
- У нас працюють найкращі майстри  манікюру, педикюру, бровісти, майстер по нарощуванню вій а також косметолог.<br>
-Ми використовуємо лише якісні матеріали, стерилізовані інструменти, одноразові розхідники та косметику преміум якості.<br>
-Наші адміністратори допоможуть обрати Вам зручний час та пригостять смачною кавою чи чаєм.<br>
-Наша команда хоче дарувати Вам красу та щиру посмішку.
-</p> </div>
+      <article>
+          <h2 class="about_us_title">Вас вітає студія краси “Shemetova studio”!</h2>
+          <p class="about_us_text">Ми працюємо з 2015 року та  даруємо красу найпрекраснішим жінкам нашого міста.<br>Чому ви маєте обрати саме НАС?!<br> У нас працюють найкращі майстри  манікюру, педикюру, бровісти, майстер по нарощуванню вій а також косметолог.<br>Ми використовуємо лише якісні матеріали, стерилізовані інструменти, одноразові розхідники та косметику преміум якості.<br>Наші адміністратори допоможуть обрати Вам зручний час та пригостять смачною кавою чи чаєм.<br>Наша команда хоче дарувати Вам красу та щиру посмішку.</p>
+      </article>
+    </div>
   </section>
 </div>
