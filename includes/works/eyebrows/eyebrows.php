697180496af46d7370f32db8f8e1332c622b0593
Alexander Kostin <asurv2@gmail.com>
2023-05-12 16:50:50 +0300

    Update code

--- includes/works/eyebrows/eyebrows.php
+++ includes/works/eyebrows/eyebrows.php
@@ -1,24 +1,14 @@
   <div class="bdt-tab-content-item " data-content-id>
     <div>
       <div data-elementor-type="section" data-elementor-id="263" class="elementor elementor-263">
-
         <?php include "eye.php"; ?>
-
-        <div class="elementor-element elementor-element-7410b99 e-con-boxed e-con" data-id="7410b99"
-          data-element_type="container" data-settings="{" content_width":"boxed"}">
-          <div class="e-con-inner">
-            <div class="elementor-element elementor-element-4c362b7 elementor-widget elementor-widget-heading"
-              data-id="4c362b7" data-element_type="widget" data-widget_type="heading.default">
+        <div>
               <div class="elementor-widget-container">
-                <h2 class="elementor-heading-title elementor-size-default">
+                <h2 class="price_heading-title elementor-heading-title elementor-size-default">
                   Бровіст</h2>
               </div>
             </div>
-          </div>
-        </div>
-
         <?php include "brows.php"; ?>
-
       </div>
     </div>
   </div>
