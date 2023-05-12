539c50507cdef1e32bc484f01eeebc208eede972
Alexander Kostin <asurv2@gmail.com>
2023-05-12 16:47:35 +0300

    Update code

--- includes/footer.php
+++ includes/footer.php
@@ -1,38 +1,41 @@
-<div class="container">
-  <div class="elementor-element elementor-element-3003fe6 e-con-full e-con" data-id="3003fe6" data-element_type="container" data-settings="{&quot;content_width&quot;:&quot;full&quot;}">
-    <div class="elementor-element elementor-element-7e2331b elementor-widget elementor-widget-shortcode" data-id="7e2331b" data-element_type="widget" data-widget_type="shortcode.default">
-      <div class="elementor-widget-container">
-        <div class="elementor-shortcode"></div>
-      </div>
-    </div>
-  </div>
+<footer class="container_main">
+<?php include "includes/parts/to-top.php" ?>
 
   <div class="elementor-element elementor-element-6f47dcc e-con-boxed" data-id="6f47dcc">
+
+
     <div style="background-color: #591A2F;" class="e-con-inner row">
       <div class="elementor-element elementor-element-a709ac0 col-md-6 col-12" data-id="a709ac0" data-element_type="container" data-settings="{" content_width":"full"}">
         <div class="elementor-element elementor-element-8bfef2a elementor-widget elementor-widget-text-editor" data-id="8bfef2a" data-element_type="widget" data-widget_type="text-editor.default">
-          <div class="elementor-widget-container">
+          <div class="elementor-widget-container footer_text">
+            <!-- <div class="logo_pack"><a href="index.html">
+                        <img src="images/logo150.webp" title="logo150" alt="logo150" width="150" height="150">
+                      </a></div> -->
             <p>Студія Краси <span style="color: #ffffff;"><a style="color: #ffffff;" href="index.html">«Shemetova
                   Studio»</a></span></p>
             <p>Троєщина</p>
-            <p>Працюємо Навіть без Світла</p>
+            <p>Працюємо Навіть без Світла</p><div class="inst_footer">
+          <?php include "includes/parts/btn_inst.php" ?></div>
           </div>
+
         </div>
       </div>
       <div class="elementor-element elementor-element-7e7450a col-md-6 col-12" data-id="7e7450a" data-element_type="container" data-settings="{" content_width":"full"}">
         <div class="elementor-element elementor-element-47a4916 elementor-widget elementor-widget-text-editor" data-id="47a4916" data-element_type="widget" data-widget_type="text-editor.default">
-          <div class="elementor-widget-container">
+          <div class="elementor-widget-container footer_text">
             <p>Адреса:</p>
-            <p>Київ Закревського 61/2 (Троєщина)</p>
-            <p><strong>Графік роботи з 09:00-21:00</strong></p>
+            <p>Київ, Закревського 61/2 (Троєщина)</p>
+            <p><strong>Графік роботи: з 09:00-21:00</strong></p>
           </div>
+            <div class="social_links-pack">
+              <?php include "includes/parts/phone_social_block.php" ?>
+            </div>
         </div>
+
       </div>
       <div class="copyright">
-  <a class="copyright_text" href="https://shemetova.studio/"> Всі права захищені © 2015 - <?php echo date('Y'); ?> Shemetova Studio</a>
-  </div>
+          <a class="copyright_text" href="https://shemetova.studio/"> Всі права захищені © 2015 - <?php echo date('Y'); ?> Shemetova Studio</a>
+      </div>
     </div>
   </div>
-
-
-</div>
+</footer>
