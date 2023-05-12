ede29e7049e655a1c394fa788709332ac781c6ae
Alexander Kostin <asurv2@gmail.com>
2023-05-12 16:46:36 +0300

    Update code

--- includes/works/manicure/manicure.php
+++ includes/works/manicure/manicure.php
@@ -1,24 +1,14 @@
 <div class="bdt-tab-content-item " data-content-id>
   <div class="price-table">
     <div data-elementor-type="section" data-elementor-id="369" class="elementor elementor-369">
-
       <?php include "manicure_main.php"; ?>
-
-      <div class="elementor-element elementor-element-1a2aace e-con-boxed e-con" data-id="1a2aace"
-        data-element_type="container" data-settings="{" content_width":"boxed"}">
-        <div class="e-con-inner">
-          <div class="elementor-element elementor-element-d64897d elementor-widget elementor-widget-heading"
-            data-id="d64897d" data-element_type="widget" data-widget_type="heading.default">
+      <div>
             <div class="elementor-widget-container">
-              <h2 class="elementor-heading-title elementor-size-default">
+              <h2 class="price_heading-title elementor-heading-title elementor-size-default">
                 Додатково</h2>
             </div>
           </div>
-        </div>
-      </div>
-
       <?php include "manicure_add.php"; ?>
-
     </div>
   </div>
 </div>
